c41dc21836d580244dcda0820a516aa89c16de51
bertirafaella <bertirafaella@gmail.com>
2026-06-18 11:20:27 -0300

    exe 8

---
+++ prova 3/lista 1/ex8.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+void merge_arquivos(char arquivo1[50], char arquivo2[50], char final[50]){
+
+    FILE *arq1, *arq2, *saida;
+    arq1=fopen("arquivo1.txt", "rt");
+    arq2=fopen("arquivo2.txt", "rt");
+    saida=fopen("final.txt", "wt");
+
+    if (arq1 == NULL || arq2 == NULL || saida == NULL) {
+        printf("Erro ao abrir arquivos.\n");
+        return;
+    }
+
+    int tem1, tem2, n1, n2;
+
+    tem1= fscanf(arq1,"%d" , &n1);
+    tem2= fscanf(arq2,"%d" , &n2);
+
+    while (tem1==1 && tem2==1){
+
+        if (n1<=n2){
+            fprintf(saida, "%d\n", n1);
+            tem1=fscanf(arq1, "%d", &n1);
+        }
+        else{
+            fprintf(saida, "%d\n", n2);
+            tem2=fscanf(arq2, "%d", &n2);
+        }
+    }
+
+     while (tem1 == 1) {
+        fprintf(saida, "%d\n", n1);
+        tem1=fscanf(arq1, "%d", &n1);
+    }
+    while (tem2 == 1) {
+        fprintf(saida, "%d\n", n2);
+        tem2=fscanf(arq2, "%d", &n2);
+    }
+    fclose(arq1);
+    fclose(arq2);
+    fclose(saida);
+}
+
+//acabei nao testando pois estava atrasada
